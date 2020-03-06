1df491af2ea8d758c1ce7f9e4031e6e783a2f4ea
svvntos <joesantos123@live.com>
2020-03-06 15:52:07 -0500

    updating files

--- README.md
+++ README.md
@@ -1,45 +1,31 @@
- # Read Me Genenrator 
-    ![](https://img.shields.io/badge/License-MIT-important)
-
-    # Description
-    Description
-
+ # Read Me Generator ![](https://img.shields.io/badge/License-MIT-important) 
+    # Description 
+     Generates a good read me
     # Table of Contents
-
-    *[Installation] (#installation)
-    *[Tests] (#tests)
-    *[Usage] (#usage)
-    *[Contributing] (#contributing)
-    *[License] (#license)
-    *[Questions] (#questions)
-
+    *[Installation](#installation)
+    *[Tests](#tests)
+    *[Usage](#usage)
+    *[Contributing](#contributing)
+    *[License](#license)
+    *[Questions](#questions)
     # Installation
     Run This Snippet to run Dependencies
-
     ```
-    Installation
+    npm i
     ```
-    
     # Tests
     Run The Tests With This Snippet
     ```
-    
+    npm test
     ```
-
     # Usage 
     Usage
-
     # Contributing
     How Users can Contribute <br />
     
-
     # Liscence <br />
     MIT
-
     # Questions
-
     If you have any Questions, You can reach me at my [Email]null
-
     <img src="https://avatars2.githubusercontent.com/u/57923603?v=4" alt="Github Avatar" style="border-radius:50px" width=100px"/>
-
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,48 +1,34 @@
-function generateMarkdown (data){
-    return ` # ${data.title} 
-    ![](https://img.shields.io/badge/License-${data.license}-important)
-
-    # Description
-    ${data.description}
-
+function generateMarkdown(data){
+    return ` # ${data.title} ![](https://img.shields.io/badge/License-${data.license}-important) 
+    # Description 
+     ${data.description}
     # Table of Contents
-
-    *[Installation] (#installation)
-    *[Tests] (#tests)
-    *[Usage] (#usage)
-    *[Contributing] (#contributing)
-    *[License] (#license)
-    *[Questions] (#questions)
-
+    *[Installation](#installation)
+    *[Tests](#tests)
+    *[Usage](#usage)
+    *[Contributing](#contributing)
+    *[License](#license)
+    *[Questions](#questions)
     # Installation
     Run This Snippet to run Dependencies
-
     \`\`\`
     ${data.installation}
     \`\`\`
-    
     # Tests
     Run The Tests With This Snippet
     \`\`\`
     ${data.tests}
     \`\`\`
-
     # Usage 
     ${data.usage}
-
     # Contributing
     How Users can Contribute <br />
     ${data.contributing}
-
     # Liscence <br />
     ${data.license}
-
     # Questions
-
     If you have any Questions, You can reach me at my [Email]${data.email}
-
     <img src="${data.avatar_url}" alt="Github Avatar" style="border-radius:50px" width=100px"/>
-
     `;
 }
 module.exports = generateMarkdown;
